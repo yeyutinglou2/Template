67e3ddcefca560c9df4f900155a599f202fbef19
duyuwei <yuwei.du@happyelements.com>
2025-09-29 18:11:18 +0800

    C205108

---
+++ KiteX/C205108/C205108/event.js
@@ -0,0 +1,102 @@
+C205108.event.ready = function () {
+    console.log('[C205108].ready');
+//    console.log('[C205108].ad: ' + JSON.stringify(kitex.data));
+    C205108.event.lottieWidgetEvent(C205108.lottie_widget);
+}
+C205108.event.viewableChange = function (viewable) {
+    if (C205108.motion != null) {
+        if (viewable) {
+            C205108.motion.start();
+        } else {
+            C205108.motion.stop();
+        }
+    }
+}
+C205108.event.makeNode = function (params) {
+    if (params.type == 'LottieView') {
+        if (params.nodeId == "widget_id") {
+            C205108.lottie_widget = new kitex.Lottie(params);
+        }
+        return;
+    }
+}
+C205108.event.widgetId = function () {
+    return C205108.widgetId;
+}
+C205108.event.lottieWidgetEvent = function (lottie) {
+    let widgetId = C205108.event.widgetId();
+    lottie.addEventListener("animationLoaded", function (event) {
+        lottie.play(0, 1, 0);
+        C205108.event.interactiveEvent(lottie);
+    });
+    lottie.addEventListener("completed", function (event) {
+        console.log("lottie.animation.completed");
+    });
+    let imageProviderPath = kitex.path + "/" + widgetId + "/images/";
+    let filepath = kitex.path + "/" + widgetId + "/" + widgetId + ".json";
+    lottie.imageProvider(imageProviderPath);
+    lottie.filepath(filepath);
+    let ad = kitex.data.ads[0];
+    let material = ad.materials[0];
+    let title = material.title;
+    let desc = material.desc;
+    if (!isValidString(title)) {
+        title = "扭动手机";
+    }
+    if (!isValidString(desc)) {
+        desc = "跳转详情页或第三方应用";
+    }
+
+    lottie.textProvider({
+        "_TEXT_ACT_01_": title,
+        "_TEXT_ACT_02_": desc
+    });
+}
+/** 互动事件处理 */
+C205108.event.interactiveEvent = function (lottie) {
+    // 互动挂件
+    //    console.log('[C205108].ad: ' + JSON.stringify(kitex.data));
+    let ad = kitex.data.ads[0];
+    let material = ad.materials[0];
+    let adSetting = ad.ad_setting;
+    let widgetId = C205108.event.widgetId();
+    let sensitivity = adSetting.sensitivity;
+    C205108.motion = new kitex.Motion(1);
+    C205108.event.motionEvent(C205108.motion);
+    // 互动组件点击
+    if (material.click_type == 1) {
+        // 挂件点击
+        let clickAreas = [
+            "_CLICK_",
+            "_CLICK_01_",
+            "_CLICK_02_",
+        ];
+        clickAreas.forEach((val, index) => {
+            lottie.addClick(val, function (params) {
+                console.log('[C205108].widget.click: ' + JSON.stringify(params));
+                kitex.ad.openByVid(params);
+            });
+        });
+    }
+}
+
+function isValidString(val) {
+    if (val == null || !(typeof val === "string") || (typeof val === "string" && val.length == 0)) {
+        return false
+    }
+    return true
+}
+
+C205108.event.motionEvent = function (motion) {
+    motion.start();
+    motion.addEventListener("start", function (params) {
+        console.log('[C205108].motion.start: ' + JSON.stringify(params));
+    });
+    motion.addEventListener("end", function (params) {
+        console.log('[C205108].motion.end: ' + JSON.stringify(params));
+    });
+    motion.addEventListener("progress", function (params) {
+        console.log('[C205108].motion.progress: ' + JSON.stringify(params));
+    });
+}
+
---
+++ KiteX/C205108/C205108/main.js
@@ -0,0 +1,27 @@
+required('C205108/event.js')
+
+var C205108 = {
+    tid: 'C205108',
+    event: {},
+    lottie_widget: undefined,
+    motion: undefined,
+    widgetId: '205108'
+}
+// 监听事件 模版环境初始化完成
+kitex.addEventListener(C205108.tid, 'ready', function () {
+    C205108.event.ready();
+})
+// 监听事件 处理自定义组件的创建
+kitex.addEventListener(C205108.tid, 'makeNode', function (params) {
+    C205108.event.makeNode(params);
+})
+kitex.addEventListener(C205108.tid, 'enterBackground', function () {
+    
+})
+kitex.addEventListener(C205108.tid, 'enterForeground', function () {
+
+})
+kitex.addEventListener(C205108.tid, 'viewableChange', function (viewable) {
+    console.log('[C205108].viewableChange: ' + JSON.stringify(viewable));
+    C205108.event.viewableChange(viewable);
+})
