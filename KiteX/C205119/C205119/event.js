6da92578e45401b6c7ff252721cdf99e2433592b
duyuwei <yuwei.du@happyelements.com>
2025-10-11 17:37:24 +0800

    update C205119

--- KiteX/C205119/C205119/event.js
+++ KiteX/C205119/C205119/event.js
@@ -1,10 +1,10 @@
 C205119.event.ready = function () {
     console.log('[C205119].ready');
-//    console.log('[C205119].ad: ' + JSON.stringify(kitex.data));
+    //    console.log('[C205119].ad: ' + JSON.stringify(kitex.data));
     C205119.event.lottieWidgetEvent(C205119.lottie_widget);
 }
 C205119.event.viewableChange = function (viewable) {
-   
+
 }
 C205119.event.makeNode = function (params) {
     if (params.type == 'LottieView') {
@@ -17,7 +17,7 @@ C205119.event.makeNode = function (params) {
 C205119.event.widgetId = function () {
     return C205119.widgetId;
 }
-C205119.event.gifImagePath = function() {
+C205119.event.gifImagePath = function () {
     let widgetId = C205119.event.widgetId();
     let imagePath = kitex.path + "/" + widgetId + "/red_packet.gif";
     return imagePath;
@@ -51,12 +51,20 @@ C205119.event.interactiveEvent = function (lottie) {
     // 互动挂件
     //    console.log('[C205119].ad: ' + JSON.stringify(kitex.data));
     let clickAreas = [
-            "_CLICK_",
-            "_CLICK_01_",
-            "_CLICK_02_",
-     ];
+        "_CLICK_",
+        "_CLICK_01_",
+        "_CLICK_02_",
+    ];
     clickAreas.forEach((val, index) => {
         lottie.addClick(val, function (params) {
+            params.dcParams.sld = '0';
+            params.dcParams.click_area = 'companion';
+            params.dcParams.down_point = '{' + params.touchEvent.downX + ',' + params.touchEvent.downY + '}';
+            params.dcParams.up_point = '{' + params.touchEvent.upX + ',' + params.touchEvent.upY + '}';
+            params.dcParams.up_timestamp = params.touchEvent.upTimestamp;
+            params.dcParams.down_timestamp = params.touchEvent.downTimestamp;
+            params.dcParams.cpt_id = C205119.widgetId;
+            params.dcParams.accpt_ids = C205119.widgetId;
             console.log('[C205119].widget.click: ' + JSON.stringify(params));
             kitex.ad.openByVid(params);
         });
