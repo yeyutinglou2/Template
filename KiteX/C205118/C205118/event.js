e0c81eae33d7b756368b63198d03a80abfff6e3e
duyuwei <yuwei.du@happyelements.com>
2025-10-11 17:33:30 +0800

    update C205118

--- KiteX/C205118/C205118/event.js
+++ KiteX/C205118/C205118/event.js
@@ -52,6 +52,14 @@ C205118.event.interactiveEvent = function (lottie) {
      ];
     clickAreas.forEach((val, index) => {
         lottie.addClick(val, function (params) {
+            params.dcParams.sld = '0';
+            params.dcParams.click_area = 'companion';
+            params.dcParams.down_point = '{' + params.touchEvent.downX + ',' + params.touchEvent.downY + '}';
+            params.dcParams.up_point = '{' + params.touchEvent.upX + ',' + params.touchEvent.upY + '}';
+            params.dcParams.up_timestamp = params.touchEvent.upTimestamp;
+            params.dcParams.down_timestamp = params.touchEvent.downTimestamp;
+            params.dcParams.cpt_id = C205118.widgetId;
+            params.dcParams.accpt_ids = C205118.widgetId;
             console.log('[C205118].widget.click: ' + JSON.stringify(params));
             kitex.ad.openByVid(params);
         });
