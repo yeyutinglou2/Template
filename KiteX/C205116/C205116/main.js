30d9ef337681fb3cd30d7353f27cc1c3ce025b45
duyuwei <yuwei.du@happyelements.com>
2025-10-11 17:10:39 +0800

    update C205116

--- KiteX/C205116/C205116/main.js
+++ KiteX/C205116/C205116/main.js
@@ -5,7 +5,8 @@ var C205116 = {
     event: {},
     lottie_widget: undefined,
     motion: undefined,
-    widgetId: '205116'
+    widgetId: '205116',
+    sld: '2'
 }
 // 监听事件 模版环境初始化完成
 kitex.addEventListener(C205116.tid, 'ready', function () {
