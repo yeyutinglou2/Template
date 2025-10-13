437d191f9f845c11c017e6db500141533b49a366
duyuwei <yuwei.du@happyelements.com>
2025-10-13 19:14:27 +0800

    update C502001

--- KiteX/C502001/C502001/event.js
+++ KiteX/C502001/C502001/event.js
@@ -2,9 +2,9 @@
 
 C502001.event.ready = function () {
     console.log('[C502001].ready');
-//    console.log('[C502001].ad: ' + JSON.stringify(kitex.data));
+    //    console.log('[C502001].ad: ' + JSON.stringify(kitex.data));
 }
-C502001.event.viewableChange = function(viewable) {
+C502001.event.viewableChange = function (viewable) {
 }
 C502001.event.makeNode = function (params) {
     if (params.type == 'Vpaid') {
@@ -15,7 +15,7 @@ C502001.event.makeNode = function (params) {
 
 
 function isValidString(val) {
-    if (val == null || !(typeof val  === "string") || (typeof val === "string" && val.length == 0)) {
+    if (val == null || !(typeof val === "string") || (typeof val === "string" && val.length == 0)) {
         return false
     }
     return true
@@ -32,37 +32,61 @@ C502001.event.vpaidInit = function (params) {
 C502001.event.vpaidReadyToPlay = function (vpaid) {
     vpaid.play();
 }
-C502001.event.vpaidPlayToEnd = function (vpaid) {
-    kitex.postMessage({
-        tid: vpaid.tid,
-        type: vpaid.type,
-        value: 'vpaidPlayToEnd',
-        params: {
-            nodeId: vpaid.nodeId
-        }
-    });
-}
+
 C502001.event.addVpaidEvent = function (vpaid) {
     vpaid.addEventListener('ready', function (params) {
         console.log('[C502001].prepareToPlay:' + JSON.stringify(params));
         C502001.event.vpaidReadyToPlay(vpaid);
-        // C502001.event.loadBg(params);
+        kitex.postMessage({
+            tid: vpaid.tid,
+            type: vpaid.type,
+            value: 'prepareToPlay',
+            params: params
+        });
     });
     vpaid.addEventListener('playStateChanged', function (params) {
         console.log('[C502001].playStateChanged:' + JSON.stringify(params));
+        kitex.postMessage({
+            tid: vpaid.tid,
+            type: vpaid.type,
+            value: 'playStateChanged',
+            params: params
+        });
     });
     vpaid.addEventListener('loadStateChanged', function (params) {
         console.log('[C502001].loadStateChanged:' + JSON.stringify(params));
+        kitex.postMessage({
+            tid: vpaid.tid,
+            type: vpaid.type,
+            value: 'loadStateChanged',
+            params: params
+        });
     });
     vpaid.addEventListener('currentTime', function (params) {
-        
+        kitex.postMessage({
+            tid: vpaid.tid,
+            type: vpaid.type,
+            value: 'currentTime',
+            params: params
+        });
     });
     vpaid.addEventListener('playEnd', function (params) {
         console.log('[C502001].playEnd:' + JSON.stringify(params));
-        C502001.event.vpaidPlayToEnd(params);
+        kitex.postMessage({
+            tid: vpaid.tid,
+            type: vpaid.type,
+            value: 'playEnd',
+            params: params
+        });
     });
     vpaid.addEventListener('error', function (params) {
         console.log('[C502001].error:' + JSON.stringify(params));
+        kitex.postMessage({
+            tid: vpaid.tid,
+            type: vpaid.type,
+            value: 'error',
+            params: params
+        });
     });
 }
 
