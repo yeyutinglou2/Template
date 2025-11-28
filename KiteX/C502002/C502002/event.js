ab39797a9cd96a89a373b672b1aaf1a3d0d96985
duyuwei <yuwei.du@happyelements.com>
2025-11-28 16:06:35 +0800

    update

--- KiteX/C502002/C502002/event.js
+++ KiteX/C502002/C502002/event.js
@@ -88,6 +88,9 @@ C502002.event.lottieBgEvent = function (lottie) {
     lottie.textProvider({
         "_FRAME_TITLE_":frameTitle
     });
+    lottie.fontWeightProvider({
+        "_FRAME_TITLE_": 700
+    });
 }
 
 
