6dbd765446afe575816ec30918ac8ea77bace5b5
duyuwei <yuwei.du@happyelements.com>
2025-11-28 16:27:42 +0800

    update

--- KiteX/C502004/C502004/event.js
+++ KiteX/C502004/C502004/event.js
@@ -2,21 +2,21 @@
 
 C502004.event.ready = function () {
     console.log('[C502004].ready');
-//    console.log('[C502004].ad: ' + JSON.stringify(kitex.data));
+    //    console.log('[C502004].ad: ' + JSON.stringify(kitex.data));
     let material = kitex.data.ads[0].materials[0];
     C502004.setting.creative_type = material.creative_type;
     if (C502004.setting.creative_type == 3) {
         C502004.setting.positionId = 'image_root_id'
         let imageSize = material.image_size;
         C502004.event.getResourceDirection(imageSize)
-    }else if (C502004.setting.creative_type == 8) {
+    } else if (C502004.setting.creative_type == 8) {
         C502004.setting.positionId = 'video_id'
         let videoSize = material.video_size;
         C502004.event.getResourceDirection(videoSize)
     }
-     C502004.event.lottieBgEvent(C502004.lottie_bg);
+    C502004.event.lottieBgEvent(C502004.lottie_bg);
 }
-C502004.event.viewableChange = function(viewable) {
+C502004.event.viewableChange = function (viewable) {
 }
 C502004.event.makeNode = function (params) {
     if (params.type == 'LottieView') {
@@ -31,7 +31,7 @@ C502004.event.makeNode = function (params) {
     }
 }
 
-C502004.event.getResourceDirection = function(params) {
+C502004.event.getResourceDirection = function (params) {
     if (params.height < params.width) {
         C502004.setting.resourceDirection = 1;
     }
@@ -39,14 +39,14 @@ C502004.event.getResourceDirection = function(params) {
     console.log('[C502004].ad: ' + C502004.setting.positionId);
 }
 
-C502004.event.bgWidgetId = function() {
+C502004.event.bgWidgetId = function () {
     return C502004.frameId;
 }
-C502004.event.cardWidgetId = function() {
+C502004.event.cardWidgetId = function () {
     return C502004.event.bgWidgetId();
 }
 
-C502004.event.bgImagePath = function() {
+C502004.event.bgImagePath = function () {
     let bgWidgetId = C502004.event.bgWidgetId();
     let imagePath = kitex.path + "/" + bgWidgetId + "/bg/" + bgWidgetId + ".jpg";
     return imagePath;
@@ -66,7 +66,7 @@ C502004.event.lottieBgEvent = function (lottie) {
     });
     let resourceName = C502004.setting.resourceDirection == 0 ? "frame_v" : "frame_h";
     let imageProviderPath = kitex.path + "/" + bgWidgetId + "/" + resourceName + "/images/";
-    let filepath = kitex.path + "/" + bgWidgetId + "/" + resourceName + "/"+ bgWidgetId + ".json";
+    let filepath = kitex.path + "/" + bgWidgetId + "/" + resourceName + "/" + bgWidgetId + ".json";
 
     lottie.imageProvider(imageProviderPath);
     lottie.filepath(filepath);
@@ -83,16 +83,19 @@ C502004.event.lottieBgEvent = function (lottie) {
     }
 
     if (!isValidString(frameTitle)) {
-            frameTitle = "今日热门推荐";
+        frameTitle = "今日热门推荐";
     }
     lottie.textProvider({
-        "_FRAME_TITLE_":frameTitle
+        "_FRAME_TITLE_": frameTitle
+    });
+    lottie.fontWeightProvider({
+        "_FRAME_TITLE_": 700
     });
 }
 
 
 function isValidString(val) {
-    if (val == null || !(typeof val  === "string") || (typeof val === "string" && val.length == 0)) {
+    if (val == null || !(typeof val === "string") || (typeof val === "string" && val.length == 0)) {
         return false
     }
     return true
