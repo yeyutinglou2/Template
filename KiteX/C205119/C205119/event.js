520c1823c21c27f589d70462f79ce131bf798696
duyuwei <yuwei.du@happyelements.com>
2025-11-28 16:23:33 +0800

    update

--- KiteX/C205119/C205119/event.js
+++ KiteX/C205119/C205119/event.js
@@ -46,7 +46,8 @@ C205119.event.lottieWidgetEvent = function (lottie) {
         "_AD_DESC_": creativeDesc
     });
     lottie.fontWeightProvider({
-        "_AD_TITLE_": 500
+        "_AD_TITLE_": 500,
+        "_CTA_TEXT_": 700
     });
 }
 /** 互动事件处理 */
